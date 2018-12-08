0e34ab9520d4e9d05431f2191b81ebd2c821a165
Steven Suh <stevenesuh@gmail.com>
2018-12-08 02:31:31 -0800

    fix animation

--- src/scripts/hoc/scrollToTop.jsx
+++ src/scripts/hoc/scrollToTop.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
 
 class ScrollToTop extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
+  componentDidUpdate({ location: prevLocation }) {
+    const { location: currLocation } = this.props;
+
+    if (currLocation !== prevLocation) {
       window.scrollTo(0, 0);
     }
   }
 
   render() {
-    return this.props.children;
+    const { children } = this.props;
+    return children;
   }
 }
+
+ScrollToTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default withRouter(ScrollToTop);
