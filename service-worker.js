7cea910b6f0fcb21aa43f1bdaa1ff24fd4d2333d
Steven Suh <stevenesuh@gmail.com>
2018-12-07 23:29:37 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/v2_stevensuh/precache-manifest.e3892e33e379dffddbdeb8dbd195ced8.js"
+  "/precache-manifest.572968501f24a5c971c3a5644a303a66.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/v2_stevensuh/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
