73a565da4d180d6214e53d18fe55633315a8f46d
Steven Suh <stevenesuh@gmail.com>
2018-12-28 09:30:16 +0900

    Updates

--- precache-manifest.e13f75b5446f2d4e93cdde1660fd0240.js
+++ precache-manifest.e13f75b5446f2d4e93cdde1660fd0240.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "bdb87a2a9cfec083daf1",
-    "url": "/static/css/main.3e0ba14c.chunk.css"
+    "revision": "fc7f074466a11d3a954f",
+    "url": "/static/css/main.3e22a72d.chunk.css"
   },
   {
-    "revision": "bdb87a2a9cfec083daf1",
-    "url": "/static/js/main.bdb87a2a.chunk.js"
+    "revision": "fc7f074466a11d3a954f",
+    "url": "/static/js/main.fc7f0744.chunk.js"
   },
   {
     "revision": "8c79e75dc77a1f16a940",
@@ -20,15 +20,19 @@ self.__precacheManifest = [
     "url": "/static/media/Amazon-Ember-Regular.76666a38.ttf"
   },
   {
-    "revision": "77a27d41ef39c0a3b4105b5c2399969b",
-    "url": "/static/media/proxima-nova-semibold.77a27d41.ttf"
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
   },
   {
     "revision": "c2519a94f580b2b2a7071446b0ed4b0b",
     "url": "/static/media/SkyhookMono.c2519a94.ttf"
   },
   {
-    "revision": "fe8d723b35442f19f4e96aef1d98f4eb",
+    "revision": "77a27d41ef39c0a3b4105b5c2399969b",
+    "url": "/static/media/proxima-nova-semibold.77a27d41.ttf"
+  },
+  {
+    "revision": "082e058688b96652b2a2843a7077e3b9",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eccfeb53fe2c37750fb6c36e2e486bd3.js"
+  "/precache-manifest.e13f75b5446f2d4e93cdde1660fd0240.js"
 );
 
 workbox.clientsClaim();
